3c0d2c6246209c1ea55314ced9d7d0f1159ca28f
mdestafadilah <desta.08b@gmail.com>
2021-10-25 13:22:15 +0700

    #3 Lesson - Kolom dengan Grid Sistem - Select, Input dan Button - Form Input

--- src/sections/details.tsx
+++ src/sections/details.tsx
@@ -1,4 +1,4 @@
-import { VStack, Heading, Text } from '@chakra-ui/react'
+import { FormControl, FormLabel, Input, VStack, Heading, Text, SimpleGrid, GridItem, Select, Checkbox, Button } from '@chakra-ui/react'
 
 const Details = () => {
     return (
@@ -7,6 +7,48 @@ const Details = () => {
                 <Heading size="2xl">Your Details</Heading>
                 <Text>If you allready account, click here to login.</Text>
             </VStack>
+            <SimpleGrid columns={2} columnGap={3} rowGap={6} w="full">
+                <GridItem colSpan={1}>
+                    <FormControl>
+                        <FormLabel>First Name</FormLabel>
+                        <Input placeholder="Desta" />
+                    </FormControl>
+                </GridItem>
+                <GridItem colSpan={1}>
+                    <FormControl>
+                        <FormLabel>Last Name</FormLabel>
+                        <Input placeholder="Fadilah" />
+                    </FormControl>
+                </GridItem>
+                <GridItem colSpan={2}>
+                    <FormControl>
+                        <FormLabel>Address</FormLabel>
+                        <Input placeholder="Jl Hacker 1 No 2" />
+                    </FormControl>
+                </GridItem>
+                <GridItem colSpan={1}>
+                    <FormControl>
+                        <FormLabel>City</FormLabel>
+                        <Input placeholder="Jakarta" />
+                    </FormControl>
+                </GridItem>
+                <GridItem colSpan={1}>
+                    <FormControl>
+                        <FormLabel>Country</FormLabel>
+                        <Select>
+                            <option value="btn">Banten</option>
+                            <option value="jkt">Jakarta</option>
+                            <option value="bdg">Bandung</option>
+                        </Select>
+                    </FormControl>
+                </GridItem>
+                <GridItem colSpan={2}>
+                    <Checkbox defaultChecked>Ship to billing address </Checkbox>
+                </GridItem>
+                <GridItem colSpan={2}>
+                    <Button size="lg" w="full">Place Order</Button>
+                </GridItem>
+            </SimpleGrid>
         </VStack>
     )
 }
